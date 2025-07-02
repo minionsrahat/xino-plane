4e77f8e5d24650e37d0e87667a08a3fa3eaa0d69
tonmoy-dev <rxdevs100@gmail.com>
2025-07-02 19:06:47 +0600

    update meeting form and test create meeting

--- packages/types/src/meeting/meeting.d.ts
+++ packages/types/src/meeting/meeting.d.ts
@@ -1,24 +1,24 @@
 export interface IUser {
   id: string;
-  name: string;
+  name?: string;
 }
 
 export interface IAgenda {
   id?: string;
   title: string;
   duration_minutes: number;
-  assignees: IUser[];
+  assignees?: IUser[];
 }
 
 export interface IMeeting {
   id?: string;
   subject: string;
   description: string;
-  chairperson: string;
+  chairperson?: IUser;
   start_time: string; // e.g., "2025-06-30 12:12:12"
   end_time: string;
-  host: IUser;
-  participants: IUser[];
-  agendas: IAgenda[];
+  host?: IUser;
+  participants?: IUser[];
+  agendas?: IAgenda[];
   attachments: File[];
 }
